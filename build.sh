a0d7e282b78b5f614bc3f6d801c4770f01bc28d2
Anandhakumarkj <anandhaak447@gmail.com>
2025-04-05 10:26:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build -t devopsproject .
+docker build -t task37 .
 echo Hyyy
 docker login -u anandhakumarkj -p Anandha@2004
-docker tag devopsproject anandhakumarkj/newdevops
-docker push anandhakumarkj/newdevops
+docker tag devopsproject anandhakumarkj/devopsproject
+docker push anandhakumarkj/devopsproject
 kubectl apply -f  deploy.yaml --validate=false
 kubectl apply -f svc.yaml --validate=false
